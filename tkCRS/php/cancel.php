4aa57ff43118f1c1e8678b86d403542c6532cff7
tkesici <20200808004@ogr.akdeniz.edu.tr>
2022-05-22 15:40:15 +0300

    added cancellation of a booking for customer

---
+++ tkCRS/php/cancel.php
@@ -0,0 +1,17 @@
+<?php
+$car = $_GET['car'];
+$conn = new mysqli("localhost", "root", "1234", "tkcrs");
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+$sql = 'UPDATE booking
+SET active = 0
+WHERE booking.id = "' . $car . '" ';
+
+if (mysqli_query($conn, $sql)) {
+    header( "refresh:0.1;url=mytransactions.php" );
+} else {
+  echo "Error updating record: " . mysqli_error($conn);
+}
+mysqli_close($conn);
+?>
